c255ff08273e950884ea24d997fd5dd8023c8dc1
Alejandro Pereira <ja.pereiragt@gmail.com>
2020-05-22 15:52:20 -0600

    Add reactjs eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['airbnb-base/legacy', 'prettier', 'plugin:vue/recommended'],
+  extends: [
+    'airbnb-base/legacy',
+    'prettier',
+    'plugin:vue/recommended',
+    'plugin:react/recommended',
+  ],
   parserOptions: {
     parser: 'babel-eslint',
     ecmaVersion: 2020,
@@ -19,17 +24,19 @@ module.exports = {
     'jsx-a11y/label-has-for': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
     'import/no-unresolved': 'off',
-    'vue/max-attributes-per-line': ['error', {
-      'singleline': 20,
-      'multiline': {
-        'max': 1,
-        'allowFirstLine': false
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
       },
-    }],
+    ],
     'vue/html-self-closing': 'off',
-    "vue/no-v-html": 'off',
-    'import/extensions': ['off']
-
+    'vue/no-v-html': 'off',
+    'import/extensions': ['off'],
   },
   settings: {
     'import/resolver': {
@@ -42,7 +49,7 @@ module.exports = {
     browser: true,
     node: true,
     jest: true,
-    jasmine: true
+    jasmine: true,
   },
   globals: {
     __WEBPACK_ENV__: true,
