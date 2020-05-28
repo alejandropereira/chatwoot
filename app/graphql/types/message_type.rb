c7fb340bf6bad5d500377353b6933dfe95644d30
Alejandro Pereira <ja.pereiragt@gmail.com>
2020-05-28 12:58:58 -0600

    Add messages query and connect to widget

--- app/graphql/types/message_type.rb
+++ app/graphql/types/message_type.rb
@@ -2,7 +2,13 @@ module Types
   class MessageType < Types::BaseObject
     field :id, ID, null: false
     field :content, String, null: true
+    field :content_type, String, null: true
     field :message_type, String, null: true
+    field :assignee, Types::UserType, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: true
+
+    def assignee
+      object.user
+    end
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,5 +3,6 @@ module Types
     field :web_widget, resolver: Queries::WebWidget
     field :agents, resolver: Queries::Agents
     field :conversations, resolver: Queries::Conversations
+    field :messages, resolver: Queries::Messages
   end
 end
